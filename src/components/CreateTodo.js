a4e70188bd78efd153c3a8210b68a2b10f8a8d18
shahriar0104 <32048867+shahriar0104@users.noreply.github.com>
2021-09-11 21:25:02 +0600

    form generalized

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -1,66 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
+import TodoFormFields from "./TodoFormFields";
 
 const CreateTodo = (props) => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [todo, setTodo] = useState('');
 
-    let formRef = React.createRef();
+    const nameChangeHandler = (event) => {
+        setName(event.target.value);
+    }
+
+    const emailChangeHandler = (event) => {
+        setEmail(event.target.value);
+    }
+
+    const todoChangeHandler = (event) => {
+        setTodo(event.target.value);
+    }
 
     const onTrigger = (event) => {
         props.addTodo(event.target);
         event.preventDefault();
-        event.target.reset();
+        setName('');
+        setEmail('');
+        setTodo('');
     }
 
     return (
         <div className="w-full sm:w-8/12 md:w-6/12 m-auto">
-            <form ref={formRef} className="bg-white shadow-lg rounded px-4 pt-6 pb-8 mb-4" onSubmit = {onTrigger}>
-                <p className="text-red-500 text-2xl font-medium text-center">Add a Todo</p>
 
-                <div className="md:flex md:items-center mt-8 mb-4 px-2 sm:px-4">
-                    <div className="md:w-1/4">
-                        <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="name">
-                            Name*
-                        </label>
-                    </div>
-                    <div className="md:w-3/4">
-                        <input required
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="name" type="text" name="name" placeholder="Name"/>
-                    </div>
-                </div>
+            <TodoFormFields title='Add a Todo' name={name} email={email} todo={todo}
+                            nameChange={nameChangeHandler} emailChange={emailChangeHandler} todoChange={todoChangeHandler}
+                            trigger={onTrigger} isCancelPresent={false}/>
 
-                <div className="md:flex md:items-center mb-4 px-2 sm:px-4">
-                    <div className="md:w-1/4">
-                        <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="email">
-                            Email*
-                        </label>
-                    </div>
-                    <div className="md:w-3/4">
-                        <input required
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="email" type="email" name="email" placeholder="Email"/>
-                    </div>
-                </div>
-
-                <div className="md:flex md:items-center mb-4 px-2 sm:px-4">
-                    <div className="md:w-1/4">
-                        <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="todo">
-                            Todo*
-                        </label>
-                    </div>
-                    <div className="md:w-3/4">
-                        <textarea required
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
-                            focus:outline-none focus:shadow-outline whitespace-pre-wrap"
-                            id="todo" cols="5" rows="3" name="todo" placeholder="Todo"/>
-                    </div>
-                </div>
-
-                <div className="flex items-center justify-center">
-                    <input
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline cursor-pointer"
-                        type="submit" value="Add Todo"/>
-                </div>
-            </form>
         </div>
     )
 }
